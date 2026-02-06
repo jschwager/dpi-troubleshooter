49804e494853ff6c5a9b2e9fe3a0c34835365184
Jared Schwager <jared.schwager@gmail.com>
2026-02-06 08:15:00 -0500

    Updated formatting

--- troubleshoot.sh
+++ troubleshoot.sh
@@ -184,10 +184,11 @@ check_modem_errors
 check_dialing_issues
 
 ## Prompt user for additional actions
+echo -e "\n${bold}=== Additional Options ===${normal}"
 echo "(1) Review recent logs"
 echo "(2) Configure Dreamcast Now profile"
 echo "(3) Exit"
-read -p $'\x0a\x0aPlease select an option above: ' -n 1 -r
+read -p $'\x0aPlease select an option above: ' -n 1 -r
 echo
 if [[ $REPLY =~ ^[1]$ ]]; then
     analyze_logs
